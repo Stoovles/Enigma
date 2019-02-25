8d839f42f6e919c6f78c1a8b63518325217e2353
Ethan Grab <ethangrab@gmail.com>
2019-02-25 03:17:17 -0700

    Added test_it_can_compile_hash to EncryptHash

--- test/encrypt_hash_test.rb
+++ test/encrypt_hash_test.rb
@@ -1,11 +1,19 @@
 require './test/test_helper'
 require './lib/enigma'
-reqiure './lib/encrypt_hash'
+require './lib/encrypt_hash'
 
 class EnigmaTest < Minitest::Test
 
   def test_it_exists
-    encrypt_hash = EncryptHash.new(key_array, date_array)
+    encrypt_hash = EncryptHash.new([], [])
 
     assert_instance_of EncryptHash, encrypt_hash
   end
+
+  def test_it_can_compile_hash
+    encrypt_hash = EncryptHash.new([], [])
+
+
+
+  end
+end
