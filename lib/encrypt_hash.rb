29a27f4da2985e9dfacf9ce67adcf3e4095d612d
Ethan Grab <ethangrab@gmail.com>
2019-02-25 03:43:37 -0700

    EncyptHash#compile_hash and test finished

--- lib/encrypt_hash.rb
+++ lib/encrypt_hash.rb
@@ -1,11 +1,19 @@
 class EncryptHash
 
+  attr_reader :key_array,
+              :date_array
+
   def initialize(key_array, date_array)
-    @key_array = []
-    @date_array = []
+    @key_array = key_array
+    @date_array = date_array
   end
 
   def compile_hash
+    int_key_array = @key_array.map {|str_num| str_num.to_i}
+    int_date_array = @date_array.map {|str_num| str_num.to_i}
+    values = [int_key_array, int_date_array].transpose.map {|x| x.reduce(:+)}
+    keys = [:A, :B, :C, :D]
+    Hash[keys.zip(values)]
   end
-
+  
 end
--- test/encrypt_hash_test.rb
+++ test/encrypt_hash_test.rb
@@ -11,9 +11,15 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_compile_hash
-    encrypt_hash = EncryptHash.new([], [])
+    encrypt_hash = EncryptHash.new(["02", "27", "71", "15"], ["1", "0", "2", "5"])
 
+    expected = {
+      A: 3,
+      B: 27,
+      C: 73,
+      D: 20
+    }
 
-
+    assert_equal expected, encrypt_hash.compile_hash
   end
 end
