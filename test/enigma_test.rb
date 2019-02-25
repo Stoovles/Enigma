429df3704734a7b5035d8797592965c0686f6650
Ethan Grab <ethangrab@gmail.com>
2019-02-24 23:47:23 -0700

    Transferred possible tests from enigma_test to key_generator,offset_generator/_tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -24,26 +24,6 @@ class EnigmaTest < Minitest::Test
     assert_equal #####, .pad_random_number
   end
 
-  def test_it_can_return_current_date
 
-
-    assert_equal "current_date.example", .current_date
-  end
-
-  def test_it_can_return_current_date_formatted_to_integers
-
-    assert_equal ######, .current_date_to_i(current_date)
-  end
-
-  def test_it_can_square_formatted_date
-
-
-    assert_equal ###########, .square(######)
-  end
-
-  def test_it_can_return_last_four_digits_of_squared_date
-
-    assert_equal ####, .last_four_digits(#################)
-  end
 
 end
---
+++ test/offset_generator_test.rb
@@ -0,0 +1,30 @@
+require './test/test_helper'
+
+class OffsetGeneratorTest < Minitest::Test
+  def setup
+
+  end
+
+  def test_it_can_return_current_date
+
+
+    assert_equal "current_date.example", .current_date
+  end
+
+  def test_it_can_return_current_date_formatted_to_integers
+
+    assert_equal ######, .current_date_to_i(current_date)
+  end
+
+  def test_it_can_square_formatted_date
+
+
+    assert_equal ###########, .square(######)
+  end
+
+  def test_it_can_return_last_four_digits_of_squared_date
+
+    assert_equal ####, .last_four_digits(#################)
+  end
+
+end
