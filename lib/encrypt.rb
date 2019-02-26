f766cf5a144c3622327240af74b0a4ee9e638c6c
Ethan Grab <ethangrab@gmail.com>
2019-02-25 18:03:21 -0700

    encrypt.rb finished

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,4 +7,5 @@ encrypted_file = File.open(ARGV[1], "w")
 message = message_file.read.gsub("\n", '')
 enigma = Enigma.new
 encrypt_hash = enigma.encrypt(message)
+encrypted_file.write("#{encrypt_hash[:encryption]}")
 puts "Created #{ARGV[1]} with the key #{encrypt_hash[:key]} and date #{encrypt_hash[:date]}"
